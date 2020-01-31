637cc635fe4bc16160b48b8ad09c0c486a866b58
shubham.si <shubham.si@BMMDVSHUBHAMSI.local>
2020-01-31 12:41:01 +0530

    commit

--- src/providers/provider.util.ts
+++ src/providers/provider.util.ts
@@ -2,18 +2,6 @@ import {ResponseFormat} from '../../source/config/Type';
 
 type configMap ={[key:string]:any;};
 
-const imageUrl={
-    "AMZP002":"https://www.google.com/search?q=amazon+images&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjKj-b4vqjnAhUK6XMBHZWUB0UQ_AUoAXoECA0QAw&biw=2560&bih=1361&dpr=1#imgrc=J2B55OP0TunymM:",
-    "APPNXP003":"https://www.google.com/search?q=appnexus&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjGvseqv6jnAhVM4zgGHbIfAlcQ_AUoA3oECBIQBQ&biw=2560&bih=1361#imgrc=WLhL8ODb0J4iDM:",
-    "OPNXP004":"https://www.google.com/search?biw=2560&bih=1361&tbm=isch&sa=1&ei=NlAxXqfBD6WYmgf1iayIDw&q=openx&oq=openx&gs_l=img.3..0l6j0i131j0l3.1623.2389..2458...0.0..0.121.483.2j3......0....1..gws-wiz-img.......0i24j0i10i24.YPZHNNlvWLw&ved=0ahUKEwintL-xv6jnAhUljOYKHfUEC_EQ4dUDCAc&uact=5#imgrc=luBXRGMcSw_otM:"
-}
-
-const imagePath ={
-    "AMZ002":"https://image.shutterstock.com/image-photo/honolulu-january-12-2017-amazon-260nw-655556107.jpg",
-    "APPNXP003":"https://martechtoday.com/wp-content/uploads/2018/08/appnexus-logo-1920x1080_fz4jy8.jpg",
-    "OPNXP004":"https://ox-prod.imgix.net/uploads/2016/11/clientLogo_OpenX.png?auto=compress",
-}
-
 const providerNames ={
     "AMZP002":"AMAZON_CREATIVE",
     "APPNXP003":"APPNEXUS_CREATIVE",
