08b4007d5811edc051235e530e7d90a606dfd6e4
shubham.si <shubham.si@BMMDVSHUBHAMSI.local>
2020-02-04 18:55:07 +0530

    commit

--- dist-server/node-serv/providers/appnexus.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var provider_util_1 = require("./provider.util");
-function sendResponseAppnexus(req, res) {
-    res.json(provider_util_1.getResponse(req.body));
-}
-exports.sendResponseAppnexus = sendResponseAppnexus;
--- dist-server/node-serv/providers/openx.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var provider_util_1 = require("./provider.util");
-function sendResponseOpenx(req, res) {
-    res.json(provider_util_1.getResponse(req.body));
-}
-exports.sendResponseOpenx = sendResponseOpenx;
